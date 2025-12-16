8255aa6bec67d307c782e040423249d831b23ec5
Tom Lagodzinski <gothrek22@users.noreply.github.com>
2025-12-16 12:40:57 +0100

    fix fetch

--- lib/utils.bash
+++ lib/utils.bash
@@ -19,14 +19,12 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 fi
 
 sort_versions() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+	sort -rV $1
 }
 
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sort -rV
+		grep -o 'refs/tags/.*' | cut -d/ -f3-
 }
 
 list_all_versions() {
